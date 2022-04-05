9a4abd28d2100a22f974ab625aad87148b7efe18
g-mei <g.mei1@outlook.com>
2022-04-05 12:43:28 +1000

    Show order costs, new OrderSeeder, updated DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            FoodSeeder::class,
+            OrderSeeder::class,
+        ]);
     }
 }
