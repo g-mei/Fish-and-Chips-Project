b6d9e5e9f1307027d883896a115940735a098d25
g-mei <g.mei1@outlook.com>
2022-05-10 11:09:47 +1000

    Missing order-submit-modal file

---
+++ resources/views/order-submit-modal.blade.php
@@ -0,0 +1,29 @@
+{{-- Submit Modal for Order --}}
+<div class="container flex justify-center mx-auto hidden modal" id="submit#{{$order->id}}">
+    <div class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50" style="z-index: 90">
+        <div class="max-w-xl p-6 bg-white rounded-md" style="z-index: 100">
+            <div class="flex items-center justify-between">
+                <h3 class="text-2xl">Submit this order?</h3>
+            </div>
+            
+            <form class="mt-5" action="{{route('submitOrder', ['id' => $order->id])}}" method="POST">
+                @method('PUT')
+                @csrf
+
+
+
+
+
+
+
+
+
+				<div class="py-6">
+                    <a href="{{route('order')}}" class="px-4 py-2 text-white bg-gray-600 rounded closeModal" type="button">Cancel</a>
+                    <button class="px-4 py-2 text-white bg-blue-600 rounded" type="submit">Save</button>
+                </div>
+            </form>
+            
+        </div>
+    </div>
+</div>
